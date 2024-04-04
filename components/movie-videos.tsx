7ef5acd729a5e9c03083809483056d06dc8a6971
박형석 <cogh@baghyeongseog-ui-MacBookPro.local>
2024-04-04 14:40:21 +0900

    deployment

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,7 +2,6 @@ import { API_URL } from "../app/(home)/page";
 import styles from "../styles/movie-video.module.css";
 
 async function getVideos(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
